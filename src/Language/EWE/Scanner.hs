17dccb184d8fadefdbaa4bafe5f9ff5ab2d008a8
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2013-09-02 22:36:07 -0500

    Preparing a new version. This will have a scanner

---
+++ src/Language/EWE/Scanner.hs
@@ -0,0 +1,15 @@
+module Language.EWE.Scanner where
+
+import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Pos(SourcePos)
+import Language.Calc.Token(Tokens, Token, Tok(..))
+import Control.Monad.State(get)
+
+pTknChar :: Char -> Tkn -> Parser Token
+pTknChar c tkn = do (,) <$> getPos <*> oneOf [c]
+
+pTknLPar = pTnkChar '(' TknLPar
+pTknRPar = pTknChar ')' TknRPar
+pTknLBrk = pTknChar '[' TknLBrk
+pTknRBrk = pTknChar ']' TknRBrk
