87f09f2032588645e1faa4acd09a196d2a613a7a
Juan Francisco Cardona Mc'Cormick <jfcmacroeafit@gmail.com>
2013-09-05 22:00:50 -0500

    Working

--- src/Language/EWE/Scanner.hs
+++ src/Language/EWE/Scanner.hs
@@ -6,11 +6,22 @@ import Text.Parsec.Pos(SourcePos)
 import Language.Calc.Token(Tokens, Token, Tok(..))
 import Control.Monad.State(get)
 
+getPos :: Parser SourcePos
+getPost = statePos <$> getParserState
+
+pTknInt :: Parser Token
+pTknInt = do ini <- getPos 
+             i  <- oneOf "123456789" 
+             is <- many digit
+             return $ (ini, TknInt (read (i:is)))
+
+
 pTknChar :: Char -> Tkn -> Parser Token
-pTknChar c tkn = do (,) <$> getPos <*> oneOf [c]
+pTknChar c tkn = do \x _ -> (x,tkn) <$> getPos <*> oneOf [c]
 
 pTknLPar, pTknRPar, pTknLBrk, pTknRBrk :: Parser Token
 pTknLPar = pTnkChar '(' TknLPar
 pTknRPar = pTknChar ')' TknRPar
 pTknLBrk = pTknChar '[' TknLBrk
 pTknRBrk = pTknChar ']' TknRBrk
+
