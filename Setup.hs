418666c4368eff79612ab4eba8b5e9c739d6cc00
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2013-10-18 13:52:30 -0500

    Adding attribute grammars

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,6 @@
-import Distribution.Simple
-main = defaultMain
+import Distribution.Simple (defaultMainWithHooks)
+import Distribution.Simple.UUAGC (uuagcLibUserHook)
+import UU.UUAGC(uuagc)
+
+main :: IO ()
+main = defaultMainWithHooks (uuagcLibUserHook uuagc)
