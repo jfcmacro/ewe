4785c950419f842a20fe50da19b3c7ffcfee1785
jfcmacro <jfcmacro@gmail.com>
2013-10-18 23:54:32 -0500

    A hard day of work

--- src/Language/EWE/Utils.hs
+++ src/Language/EWE/Utils.hs
@@ -1,7 +1,8 @@
 module Language.EWE.Utils(emptyProg,
                           equates,
                           stms,
-                          convert) where
+                          (<-->),
+                          isMemRefId) where
 
 import Language.EWE.AbsSyn
 
@@ -14,8 +15,13 @@ equates (Prg _ e) = e
 stms :: Prog -> Stmts
 stms (Prg s _) = s
 
-convert :: Either String () -> Either String () -> Either String ()
-convert (Left s) (Left s') = Left (s ++ s')
-convert (Left s) _         = Left s
-convert _        (Left s') = Left s'
-convert _        _         = Right ()
+infixl 5 <-->
+
+(<-->) :: Either String () -> Either String () -> Either String ()
+(Left s) <--> (Left s') = Left (s ++ s')
+(Left s) <--> _         = Left s
+_       <--> (Left s')  = Left s'
+_       <--> a          = a
+
+isMemRefId :: MemRef -> Bool
+isMemRefId (MRefId _) = True
